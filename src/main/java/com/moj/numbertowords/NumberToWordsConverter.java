7f8dab8bd886ccef26716a77fa7d877966cabaf2
Nitin Prabhu <prabhunitin61@gmail.com>
2017-01-18 01:22:07 +0000

    Test case to handle digits between 0 and 1 billion

--- src/main/java/com/moj/numbertowords/NumberToWordsConverter.java
+++ src/main/java/com/moj/numbertowords/NumberToWordsConverter.java
@@ -23,7 +23,6 @@ public class NumberToWordsConverter
 									"seventeen",
 									"eighteen",
 									"nineteen"};
-
 	private static final String[] MULTIPLES_OF_10 = {
 									"",
 									"",
@@ -35,30 +34,113 @@ public class NumberToWordsConverter
 									"seventy",
 									"eighty",
 									"ninety"};
+	private int number = 0;
+	private StringBuffer result = new StringBuffer();
 
 	public static String convert(final int number)
 	{
-		if (number >= 100)
+		return new NumberToWordsConverter(number).convert();
+	}
+
+	public NumberToWordsConverter(final int number)
+	{
+		this.number = number;
+	}
+
+	public String convert()
+	{
+		if (number == 0)
 		{
-			return NUMBERS_UP_TO_19[number / 100] + " hundred";
+			return ("zero");
 		}
-		else if (number < 20)
+		else if (number > 999999999)
 		{
-			return NUMBERS_UP_TO_19[number];
+			throw new IllegalArgumentException();
 		}
-		else
+
+		final int millions = number / 1000000;
+		int remainder = number - millions * 1000000;
+		final int thousands = remainder / 1000;
+		remainder = remainder - thousands * 1000;
+		final int hundreds = remainder / 100;
+		final int tensAndUnits = remainder - hundreds * 100;
+
+		appendMillions(millions);
+		appendThousands(thousands);
+		appendHundreds(hundreds);
+		appendTensAndUnits(tensAndUnits);
+		return result.toString();
+	}
+
+	private void appendMillions(final int millions)
+	{
+		if (millions > 0)
 		{
-			final int tens = number / 10;
-			final int units = number - tens * 10;
-			if (units == 0)
-			{
-				return MULTIPLES_OF_10[tens];
-			}
-			else
-			{
-				return MULTIPLES_OF_10[tens] + "-" + NUMBERS_UP_TO_19[units];
-			}
+			NumberToWordsConverter millionsConvertor = new NumberToWordsConverter(millions);
+			append(millionsConvertor.convert() + " million");
 		}
 	}
 
+	private void appendThousands(final int thousands)
+	{
+		if (thousands > 0)
+		{
+			NumberToWordsConverter thousandsConvertor = new NumberToWordsConverter(thousands);
+			append(thousandsConvertor.convert() + " thousand");
+		}
+	}
+
+	private void appendHundreds(final int hundreds)
+	{
+		if (hundreds > 0)
+		{
+			append(NUMBERS_UP_TO_19[hundreds] + " hundred");
+		}
+	}
+
+	private void appendTensAndUnits(final int tensAndUnits)
+	{
+		if (tensAndUnits > 0)
+		{
+			appendWithAnd(convertTensAndUnits(tensAndUnits));
+		}
+	}
+
+	private void append(final String words)
+	{
+		appendWithSeparator(words, " ");
+	}
+
+	private void appendWithAnd(final String words)
+	{
+		appendWithSeparator(words, " and ");
+	}
+
+	private void appendWithSeparator(final String words, final String separator)
+	{
+		if (result.length() > 0)
+		{
+			result.append(separator);
+		}
+		result.append(words);
+	}
+
+	private String convertTensAndUnits(final int number)
+	{
+		final int tens = number / 10;
+		final int units = number - tens * 10;
+
+		if (number < 20)
+		{
+			return NUMBERS_UP_TO_19[number];
+		}
+		else if (units == 0)
+		{
+			return MULTIPLES_OF_10[tens];
+		}
+		else
+		{
+			return MULTIPLES_OF_10[tens] + "-" + NUMBERS_UP_TO_19[units];
+		}
+	}
 }
--- src/test/java/com/moj/numbertowords/test/NumberToWordsCoverterTest.java
+++ src/test/java/com/moj/numbertowords/test/NumberToWordsCoverterTest.java
@@ -67,7 +67,66 @@ public class NumberToWordsCoverterTest
 	public void convertsHundreds()
 	{
 		assertNumberToWordsConversion(100, "one hundred");
-		assertNumberToWordsConversion(200, "three hundred");
+		assertNumberToWordsConversion(200, "two hundred");
+	}
+
+	@Test
+	public void convertsArbitraryThreeDigitNumbers()
+	{
+		assertNumberToWordsConversion(243, "two hundred and forty-three");
+		assertNumberToWordsConversion(360, "three hundred and sixty");
+		assertNumberToWordsConversion(405, "four hundred and five");
+	}
+
+	@Test
+	public void convertsThousands()
+	{
+		assertNumberToWordsConversion(4000, "four thousand");
+		assertNumberToWordsConversion(9000, "nine thousand");
+	}
+
+	@Test
+	public void convertsArbitraryFourDigitNumbers()
+	{
+		expectConversion(1234, "one thousand two hundred and thirty-four");
+		expectConversion(9600, "nine thousand six hundred");
+		expectConversion(2050, "two thousand and fifty");
+		expectConversion(7008, "seven thousand and eight");
+	}
+
+	@Test
+	public void convertsArbitraryFiveAndSixDigitNumbers()
+	{
+		expectConversion(123456, "one hundred and twenty-three thousand four hundred and fifty-six");
+		expectConversion(200003, "two hundred thousand and three");
+		expectConversion(340000, "three hundred and forty thousand");
+		expectConversion(400500, "four hundred thousand five hundred");
+		expectConversion(600070, "six hundred thousand and seventy");
+		expectConversion(800000, "eight hundred thousand");
+		expectConversion(90000, "ninety thousand");
+		expectConversion(10020, "ten thousand and twenty");
+		expectConversion(12300, "twelve thousand three hundred");
+	}
+
+	@Test
+	public void convertsArbitrarySevenEightAndNineDigitNumbers()
+	{
+		expectConversion(123456789, "one hundred and twenty-three million four hundred and fifty-six thousand seven hundred and eighty-nine");
+		expectConversion(100000002, "one hundred million and two");
+		expectConversion(345000067, "three hundred and forty-five million and sixty-seven");
+		expectConversion(12300400, "twelve million three hundred thousand four hundred");
+		expectConversion(7000000, "seven million");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void rejectsNumbersOver999999999()
+	{
+		NumberToWordsConverter.convert(1000000000);
+	}
+
+	private void expectConversion(final int number, final String expectedWords)
+	{
+		assertEquals(expectedWords, NumberToWordsConverter.convert(number));
 	}
 
 	private void assertNumberToWordsConversion(final int number, final String expectedWords)
